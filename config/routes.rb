9c57678de64408105c7e4dda4c1f4e5b89ed9b9f
jtaylorjohnson <jtaylorjohnson@gmail.com>
2021-09-07 15:57:55 -0500

    views and resource nesting

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
 
   delete '/logout' => 'sessions#destroy'
 
-  resources :plants
-  resources :users
+  resources :plants do
+    resources :comments, only: [:new]
+  resources :users do
+    resources :plants, only: [:index]
   resources :comments
   resources :user_plants
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
