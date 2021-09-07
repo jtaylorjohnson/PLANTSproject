dc31ed214659d00bf01649059cd63661f4e0c1b4
jtaylorjohnson <jtaylorjohnson@gmail.com>
2021-09-06 20:55:26 -0500

    migrated

--- db/migrate/20210907012945_create_user_plants.rb
+++ db/migrate/20210907012945_create_user_plants.rb
@@ -1,8 +1,8 @@
 class CreateUserPlants < ActiveRecord::Migration[6.0]
   def change
     create_table :user_plants do |t|
-      t.resources :user
-      t.resources :plant
+      t.references :user, null: false, foreign_key: true
+      t.references :plant, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20210907013111_create_comments.rb
+++ db/migrate/20210907013111_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.string :content
-      t.resources :user
-      t.resources :plant
+      t.references :user, foreign_key: true
+      t.references :plant, foreign_key: true
 
       t.timestamps
     end
